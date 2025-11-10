f0aedfee9ef8af06355eb8dbd33dd2b97c70d2d1
QuantumCoder325 <trevor.choi.tech@outlook.com>
2025-11-10 06:09:28 -0500

    Test if AdminLoginUnitTest can fail

--- OTAMS_project_with_CircleCI/app/src/androidTest/java/com/example/otams_project/AdminLoginUnitTest.java
+++ OTAMS_project_with_CircleCI/app/src/androidTest/java/com/example/otams_project/AdminLoginUnitTest.java
@@ -6,25 +6,35 @@ import androidx.test.filters.LargeTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class AdminLoginUnitTest implements Login {
 
+    CountDownLatch latch;
+
     @Test
-    public void adminCredentialsAndLoginAreFunctional() {
+    public void adminCredentialsAndLoginAreFunctional() throws InterruptedException {
         FirebaseAccessor accessor = FirebaseAccessor.getInstance();
         String email = "admin.root@gmail.com";
         String password = "admin123@";
         accessor.doesEmailMatchPassword(this, email, password);
+
+        latch = new CountDownLatch(1);
+
+        boolean completed = latch.await(5, TimeUnit.SECONDS);
+        assert  completed: ("Login failed");
     }
 
     @Override
     public void denySignIn() {
-        assert false;
+
     }
 
     @Override
     public void approveSignIn(Account account) {
-        assert false;
+        latch.countDown();
     }
 }
